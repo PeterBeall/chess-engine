6d43caa87f27f1ed218e2bf96f92009e2a851199
PeterBeall <PeterBeall84@gmail.com>
2020-11-10 11:34:12 -0700

    made Queen less terrible by replacing duplicate code

--- Queen.java
+++ Queen.java
@@ -1,88 +1,17 @@
 public class Queen extends Piece {
+	//I'll build on these to determine the Queen's legal moves:
+	Bishop bishop;
+	Rook rook;
+	
 	public Queen(boolean white, Board board) {
 		super(white, "Q", board);
+		
+		bishop = new Bishop(white, board);
+		rook = new Rook(white, board);
 	}
 	
 	public boolean isLegalMove(int fromRank, int fromFile, int toRank, int toFile) {
-		//This can move either like a bishop or like a rook.
-		boolean likeBishop = true;
-		//Bishops move diagonally (i.e. don't not move diagonally):
-		if ((fromRank == toRank && fromFile == toFile) || !(Math.abs(fromRank-toRank) == Math.abs(fromFile-toFile))) {
-			likeBishop = false;
-		}else {
-			//But, there can't be any pieces in the way:
-			int greaterRank = fromRank;
-			int lesserRank = toRank;
-			if (fromRank < toRank) {
-				greaterRank = toRank;
-				lesserRank = fromRank;
-			}
-			
-			int greaterFile = fromFile;
-			int lesserFile = toFile;
-			if (fromFile < toFile) {
-				greaterFile = toFile;
-				lesserFile = fromFile;
-			}
-			
-			for (int i=greaterFile-1; lesserFile<i; i--) {
-				if ((fromFile==greaterFile && fromRank==greaterRank) || (fromFile==lesserFile && fromRank==lesserRank)) {
-					for (int j=greaterRank-1; lesserRank<j; j--) {
-						if (!myBoard.pieces[i][j].empty) {
-							likeBishop = false;
-						}
-					}
-				}else {
-					for (int j=lesserRank+1; greaterRank>j; j++) {
-						if (!myBoard.pieces[i][j].empty) {
-							likeBishop = false;
-						}
-					}
-				}
-			}
-		}
-		if (likeBishop) {
-			return true;
-		}
-		
-		//It didn't move like a bishop, so we consider the rook-like moves.
-		
-		//Rooks move like so; one of the coordinates changes and the other stays the same:
-		if ((fromRank == toRank && fromFile == toFile) || (fromRank != toRank && fromFile != toFile)) {
-			return false;
-		}
-		
-		//But, there can't be any pieces in the way:
-		int greater;
-		int lesser;
-		if (fromRank == toRank) {
-			greater = fromFile;
-			lesser = toFile;
-			if (fromFile < toFile) {
-				greater = toFile;
-				lesser = fromFile;
-			}
-			
-			for (int i=greater-1; lesser<i; i--) {
-				if (!myBoard.pieces[fromRank][i].empty) {
-					return false;
-				}
-			}
-		}else {
-			greater = fromRank;
-			lesser = toRank;
-			if (fromRank < toRank) {
-				greater = toRank;
-				lesser = fromRank;
-			}
-			
-			for (int i=greater-1; lesser<i; i--) {
-				if (!myBoard.pieces[i][fromFile].empty) {
-					return false;
-				}
-			}
-		}
-		
-		return true;
+		//the Queen can move like a bishop, or like a rook:
+		return bishop.isLegalMove(fromRank, fromFile, toRank, toFile) || rook.isLegalMove(fromRank, fromFile, toRank, toFile);
 	}
 }
